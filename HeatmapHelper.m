6ff406e03f48be095b84a984e62d348b9019e383
yyakult <xielan@pku.edu.cn>
2016-11-03 10:01:42 +0800

    统计heatmap中，经度纬度的观看概率分布

---
+++ HeatmapHelper.m
@@ -0,0 +1,33 @@
+clear
+clc
+I1 = imread('heatmap.png');
+I2 = rgb2gray(I1);
+I2 = double(I2) / 255;
+[h,w] = size(I2);
+rowsum = zeros(h,1);
+colsum = zeros(w,1);
+sum = 0;
+for i = 1:1:h
+    for j = 1:1:w
+        rowsum(i) = rowsum(i) + I2(i,j);
+        sum = sum + I2(i,j);
+    end
+end
+
+for i = 1:1:w
+    for j = 1:1:h
+        colsum(i) = colsum(i) + I2(j,i);
+    end
+end
+
+for i = 1:1:h
+    rowsum(i) = rowsum(i) / sum;
+end
+
+for i = 1:1:w
+    colsum(i) = colsum(i) / sum;
+end
+
+
+
+
